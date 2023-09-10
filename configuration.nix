81a916cd45e361a2f47494d739c5b0e0067d0f19
Beat Hagenlocher <bah@posteo.de>
2023-09-10 21:41:48 +0200

    Change configuration for eriador

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "numenor"; # Define your hostname.
+  networking.hostName = "eriador"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
