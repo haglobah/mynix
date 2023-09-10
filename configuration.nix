7b736c808bf921b60ac711a29f9d30d7f0a84f1a
Beat Hagenlocher <bah@posteo.de>
2023-09-10 22:06:43 +0200

    Add proper uri for storage box

--- configuration.nix
+++ configuration.nix
@@ -121,13 +121,13 @@
   ];
 
   fileSystems."/mnt/share" = {
-    device = "//<IP_OR_HOST>/path/to/share";
+    device = "//u366465.your-storagebox.de/backup";
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-    in ["${automount_opts},credentials=./smb-secrets"];
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
   };
 
   # Some programs need SUID wrappers, can be configured further or are
