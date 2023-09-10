08445281e1a52166655a9b4625caf2886a9d1fcf
Beat Hagenlocher <bah@posteo.de>
2023-09-10 22:41:33 +0200

    Mount box as user

--- configuration.nix
+++ configuration.nix
@@ -127,7 +127,7 @@
       # this line prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
   };
 
   # Some programs need SUID wrappers, can be configured further or are
