196965382b38821939863e04190fdd418852325b
Beat Hagenlocher <bah@posteo.de>
2023-12-05 16:01:39 +0100

    Move gnome extensions to home-manager

--- configuration.nix
+++ configuration.nix
@@ -132,9 +132,6 @@
     git
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     home-manager
-    gnome.gnome-terminal
-    gnomeExtensions.pop-shell
-    gnome.gnome-tweaks
 
     # doom emacs
     ripgrep
