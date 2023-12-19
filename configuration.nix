c986462bd8b92460ef1a80a3e44a7e6e5ce7fa91
Beat Hagenlocher <bah@posteo.de>
2023-12-19 14:46:21 +0100

    Update system

--- configuration.nix
+++ configuration.nix
@@ -110,15 +110,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    git
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    emacs
     home-manager
-    gnome.gnome-terminal
-
-    # doom emacs
-    ripgrep
-    fd
 
     # Samba for Storage Box
     cifs-utils
