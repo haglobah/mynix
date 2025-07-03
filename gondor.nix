76c78c19ccca745b777fa703afa5198520268642
Beat Hagenlocher <bah@posteo.de>
2025-07-03 12:19:14 +0200

    Fix school network issues
    15s trying to connect via IPv6, failing, then falling back to
    IPv4 (which works)
    
    - Set cloudflare and google as nameservers (cause the default one
      has this problem)
    - Disable networkmanager dns (otherwise 192.168.50.1 would be set as
      the first one)

--- gondor.nix
+++ gondor.nix
@@ -3,6 +3,12 @@
   config = {
     networking.hostName = "gondor";
 
+    networking.nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
+    ];
+    networking.networkmanager.dns = "none";
+
     environment.systemPackages = [
     ];
   };
