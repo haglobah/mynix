d07dbd41df562197c4412d0be52d6d4bd81a77f4
Beat Hagenlocher <bah@posteo.de>
2026-01-20 11:21:56 +0100

    Downgrade wireplumber package

--- flake.nix
+++ flake.nix
@@ -54,6 +54,19 @@
         config.allowUnfreePredicate = (_: true);
         # config.nix.settings.extra-deprecated-features = [ "url-literals" ];
         overlays = [
+          (_final: prev: {
+            # https://github.com/NixOS/nixpkgs/issues/475202
+            wireplumber = prev.wireplumber.overrideAttrs (_old: rec {
+              version = "0.5.12";
+              src = pkgs.fetchFromGitLab {
+                domain = "gitlab.freedesktop.org";
+                owner = "pipewire";
+                repo = "wireplumber";
+                rev = version;
+                hash = "sha256-3LdERBiPXal+OF7tgguJcVXrqycBSmD3psFzn4z5krY=";
+              };
+            });
+          })
           # use lix instead of nix
           # (final: prev: {
           #   inherit (final.lixPackageSets.stable)
