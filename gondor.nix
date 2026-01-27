f4681cbad26173d3818577c1b484ba79d96ceb9b
Beat Hagenlocher <bah@posteo.de>
2026-01-27 15:48:13 +0100

    Remove roßleben school network fix for now

--- gondor.nix
+++ gondor.nix
@@ -6,15 +6,16 @@
     boot.kernelParams = [
       # router can't do ipv6 at home
       "ipv6.disable=1"
-    # https://www.reddit.com/r/framework/comments/1goh7hc/anyone_else_get_this_screen_flickering_issue/
+      # https://www.reddit.com/r/framework/comments/1goh7hc/anyone_else_get_this_screen_flickering_issue/
       "amdgpu.dcdebugmask=0x410"
     ];
 
-    networking.nameservers = [
-      "1.1.1.1"
-      "8.8.8.8"
-    ];
-    networking.networkmanager.dns = "none";
+    # Makes Roßleben school network work
+    # networking.nameservers = [
+    #   "1.1.1.1"
+    #   "8.8.8.8"
+    # ];
+    # networking.networkmanager.dns = "none";
 
     environment.systemPackages = [
     ];
